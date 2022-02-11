14ce7e7a889c733cf223896983e94b7968d9be83
Thomas Freeman <tlfree87@gmail.com>
2022-02-11 18:33:55 -0500

    Made install_all.sh more robust

--- install_all.sh
+++ install_all.sh
@@ -2,8 +2,11 @@
 
 # Run docker compose up -d in each of the directories below the current directory
 
-for dir in */; do
-    cd $dir
-    docker compose up -d
-    cd ..
+for item in *; do
+    if [ -d "$item" ]; then
+        echo "$item"
+	cd $dir
+	docker compose up -d
+	cd ..
+    fi
 done
